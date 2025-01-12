92a2b6b851efc336ef7933ee8010439009ff382c
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-12 00:33:30 -0300

    created theme config to set body texts to black

--- lib/ui/providers/theme_provider.dart
+++ lib/ui/providers/theme_provider.dart
@@ -4,17 +4,25 @@ import 'package:flutter/material.dart';
 final themeProvider = StateProvider<bool>((ref) => false);
 
 final ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  primarySwatch: Colors.blue,
-  appBarTheme: AppBarTheme(
-    foregroundColor: Colors.white,
-  ),
-);
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
+    appBarTheme: AppBarTheme(
+      foregroundColor: Colors.white,
+    ),
+    textTheme: TextTheme(
+      bodyMedium: TextStyle(
+        color: Colors.black,
+      ),
+    ));
 
 final ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  primarySwatch: Colors.blueGrey,
-  appBarTheme: AppBarTheme(
-    foregroundColor: Colors.white,
-  ),
-);
+    brightness: Brightness.dark,
+    primarySwatch: Colors.blueGrey,
+    appBarTheme: AppBarTheme(
+      foregroundColor: Colors.white,
+    ),
+    textTheme: TextTheme(
+      bodyMedium: TextStyle(
+        color: Colors.black,
+      ),
+    ));
