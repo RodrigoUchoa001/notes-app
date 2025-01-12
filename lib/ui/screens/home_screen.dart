0f77a0bd8400fe9f3ee0e6f4bd967c5acec85eef
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-12 00:06:46 -0300

    altered appbar style

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -8,11 +8,25 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Notes"),
+        toolbarHeight: 86,
+        backgroundColor: Colors.transparent,
+        titleSpacing: 24,
+        title: const Text(
+          "Notes",
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+        ),
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.search),
+            icon: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade800,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: EdgeInsets.all(8),
+              margin: EdgeInsets.only(right: 12), // half title spacing
+              child: Icon(Icons.search),
+            ),
           ),
         ],
       ),
