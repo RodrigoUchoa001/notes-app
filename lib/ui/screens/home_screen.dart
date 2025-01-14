9d6aa3114ad5ee6bcb90811db53ce45678f0db8a
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-13 23:13:51 -0300

    improved home screen dimentions using project on figma

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notes_app/ui/screens/edit_note_screen.dart';
 import 'package:notes_app/ui/widgets/home_screen/notes.dart';
 
@@ -11,65 +12,78 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         toolbarHeight: 86,
         backgroundColor: Colors.transparent,
-        titleSpacing: 24,
-        title: const Text(
+        titleSpacing: 25,
+        title: Text(
           "Notes",
-          style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+          style: GoogleFonts.nunito(
+            fontSize: 43,
+            fontWeight: FontWeight.w600,
+          ),
         ),
         actions: [
           Material(
-            color: Colors.grey
-                .shade800, // set color here, so the inkwell animation appears
-            borderRadius: BorderRadius.circular(12),
+            color: Color(
+                0xFF3B3B3B), // set color here, so the inkwell animation appears
+            borderRadius: BorderRadius.circular(15),
             child: InkWell(
               onTap: () {},
-              borderRadius: BorderRadius.circular(12),
+              borderRadius: BorderRadius.circular(15),
               child: Container(
-                height: 42,
-                width: 42,
+                height: 50,
+                width: 50,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(15),
                 ),
                 child: Center(
-                  child: Icon(Icons.search),
+                  child: Icon(Icons.search, size: 24),
                 ),
               ),
             ),
           ),
-          const SizedBox(width: 24),
+          const SizedBox(width: 25),
         ],
       ),
       body: SafeArea(
         minimum: EdgeInsets.symmetric(horizontal: 20),
         child: Notes(),
       ),
-      floatingActionButton: Material(
-        color: Colors
-            .grey.shade800, // set color here, so the inkwell animation appears
-        borderRadius: BorderRadius.circular(32),
-        child: InkWell(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => const EditNoteScreen(
-                  backgroundColor: Colors.black,
-                  titleText: '',
-                  contentText: '',
-                  dateText: '',
-                ),
+      floatingActionButton: InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const EditNoteScreen(
+                backgroundColor: Colors.black,
+                titleText: '',
+                contentText: '',
+                dateText: '',
               ),
-            );
-          },
-          borderRadius: BorderRadius.circular(32),
-          child: Container(
-            height: 52,
-            width: 52,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
             ),
-            child: Center(
-              child: Icon(Icons.add, size: 32),
-            ),
+          );
+        },
+        borderRadius: BorderRadius.circular(72),
+        child: Container(
+          height: 70,
+          width: 70,
+          decoration: BoxDecoration(
+            color: Color(0xFF252525),
+            borderRadius: BorderRadius.circular(72),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0x40000000),
+                spreadRadius: 0,
+                blurRadius: 10,
+                offset: Offset(0, 5),
+              ),
+              BoxShadow(
+                color: Color(0x40000000),
+                spreadRadius: 0,
+                blurRadius: 10,
+                offset: Offset(-5, 0),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Icon(Icons.add, size: 48),
           ),
         ),
       ),
