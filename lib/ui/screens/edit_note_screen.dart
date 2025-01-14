9ebdad2d63a847c536b6f9856f8b34cf34cd1ebb
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-13 21:16:37 -0300

    started edit note screen

--- lib/ui/screens/edit_note_screen.dart
+++ lib/ui/screens/edit_note_screen.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
+class NoteFields {
+  final String titleController;
+  final String dateController;
+  final String contentController;
+
+  NoteFields(this.titleController, this.dateController, this.contentController);
+}
+
 class EditNoteScreen extends StatelessWidget {
-  const EditNoteScreen({super.key});
+  final String titleText;
+  final String dateText;
+  final String contentText;
+  const EditNoteScreen(
+      {super.key,
+      required this.titleText,
+      required this.contentText,
+      required this.dateText});
 
   @override
   Widget build(BuildContext context) {
+    final titleController = TextEditingController(text: titleText);
+    final contentController = TextEditingController(text: contentText);
+    final dateController = TextEditingController(text: dateText);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        titleSpacing: 24,
+        toolbarHeight: 86,
         actions: [
           Material(
             color: Colors.grey
@@ -31,6 +50,33 @@ class EditNoteScreen extends StatelessWidget {
           ),
           const SizedBox(width: 24),
         ],
+      ),
+      body: SafeArea(
+        minimum: EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: [
+            TextFormField(
+              decoration: InputDecoration.collapsed(hintText: ''),
+              maxLines: null,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              keyboardType: TextInputType.multiline,
+              controller: titleController,
+            ),
+            const SizedBox(height: 16),
+            Text(dateController.text, style: TextStyle(color: Colors.white54)),
+            const SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration.collapsed(hintText: ''),
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              controller: contentController,
+            ),
+            const SizedBox(height: 24),
+          ],
+        ),
       ),
     );
   }
