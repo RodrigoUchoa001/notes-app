6d041b0e7f8a3a7a7be49d56f824810da003e69f
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-20 20:57:10 -0300

    showing user icon in the home screen

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:notes_app/ui/screens/edit_note_screen.dart';
+import 'package:notes_app/ui/screens/user_info_screen.dart';
 import 'package:notes_app/ui/widgets/app_bar_button.dart';
 import 'package:notes_app/ui/widgets/home_screen/notes.dart';
 
@@ -26,7 +28,32 @@ class HomeScreen extends StatelessWidget {
             function: () {},
             icon: Icons.search,
           ),
-          const SizedBox(width: 25),
+          const SizedBox(width: 12),
+          FutureBuilder(
+            future: Future.value(FirebaseAuth.instance.currentUser),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final currentUser = snapshot.data!;
+                return IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => UserInfoScreen(),
+                      ),
+                    );
+                  },
+                  icon: CircleAvatar(
+                    foregroundImage: NetworkImage(currentUser.photoURL ?? ''),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return const Icon(Icons.error);
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
+          const SizedBox(width: 12),
         ],
       ),
       body: SafeArea(
