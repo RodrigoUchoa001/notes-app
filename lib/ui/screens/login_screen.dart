b40c97bb006d65a315191c73c3c3746067a9bb48
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-21 21:24:22 -0300

    fixed typo

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:notes_app/controllers/user_controller.dart';
-import 'package:notes_app/ui/providers/is_loggin_in_provider.dart';
+import 'package:notes_app/ui/providers/is_logging_in_provider.dart';
 import 'package:notes_app/ui/screens/home_screen.dart';
 
 class LoginScreen extends ConsumerWidget {
