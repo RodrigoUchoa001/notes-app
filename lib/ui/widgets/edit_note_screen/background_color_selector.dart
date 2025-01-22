fc617356eb1b16cc4d22aee153380c9a238e0fc7
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-21 21:05:42 -0300

    created widget for select the background color of the note

---
+++ lib/ui/widgets/edit_note_screen/background_color_selector.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes_app/ui/providers/note_background_color_provider.dart';
+
+class BackgroundColorSelector extends ConsumerWidget {
+  BackgroundColorSelector({super.key});
+
+  final colors = [
+    Color(0xFF252525),
+    Colors.redAccent.shade100,
+    Colors.lightBlueAccent.shade100,
+    Colors.purple.shade300,
+    Colors.pinkAccent.shade100,
+    Colors.green.shade300,
+  ];
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final backgroundColor = ref.watch(noteBackgroundColorProvider);
+
+    return Container(
+      color: Color(0xFF252525),
+      child: Row(
+        children: colors.map(
+          (color) {
+            return IconButton(
+              onPressed: () {
+                ref.read(noteBackgroundColorProvider.notifier).state = color;
+              },
+              icon: Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  border: Border.all(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            );
+          },
+        ).toList(),
+      ),
+    );
+  }
+}
