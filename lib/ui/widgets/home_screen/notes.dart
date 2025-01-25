f00bc8c6e358b1206f67f58cb632b66527da0743
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-24 22:59:39 -0300

    home screen is now listing the notes from firestore

--- lib/ui/widgets/home_screen/notes.dart
+++ lib/ui/widgets/home_screen/notes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/controllers/note_controller.dart';
 import 'package:notes_app/ui/widgets/home_screen/note_card.dart';
 import 'package:waterfall_flow/waterfall_flow.dart';
 
@@ -47,20 +48,40 @@ class Notes extends StatelessWidget {
       },
     ];
 
-    return WaterfallFlow.builder(
-      gridDelegate: const SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // Número de colunas
-        crossAxisSpacing: 12, // Espaçamento horizontal
-        mainAxisSpacing: 12, // Espaçamento vertical
-      ),
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        final item = items[index];
-        return NoteCard(
-          color: item['color'] as Color,
-          title: item['title'] as String,
-          date: item['date'] as String,
-          content: item['content'] as String,
+    return FutureBuilder(
+      future: NoteController().getNotes(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text("ERRO: ${snapshot.error.toString()}"),
+          );
+        } else if (snapshot.data!.isEmpty) {
+          return Center(
+            child: const Text("NENHUMA NOTA"),
+          );
+        }
+        return WaterfallFlow.builder(
+          gridDelegate:
+              const SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // Número de colunas
+            crossAxisSpacing: 12, // Espaçamento horizontal
+            mainAxisSpacing: 12, // Espaçamento vertical
+          ),
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, index) {
+            final data = snapshot.data![index];
+            return NoteCard(
+              noteId: data.noteId,
+              title: data.title,
+              content: data.content,
+              color: data.color,
+              date: data.dateToString(),
+            );
+          },
         );
       },
     );
