6dbd478005d3c4df9eb8f36fca7854f4ecfb166e
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-24 23:03:45 -0300

    separated functions that get constrasting text color from another color

---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// get a contransting color to use in texts
+Color getContrastingTextColor(Color color) {
+  double luminance = color.computeLuminance();
+  return (luminance + 0.1) > 0.5 ? Colors.black : Colors.white;
+}
