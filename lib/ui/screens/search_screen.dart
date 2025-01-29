4904a50225357cb6183642d257949c6064b24852
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-29 20:43:34 -0300

    added hero animation on notes on search screen

--- lib/ui/screens/search_screen.dart
+++ lib/ui/screens/search_screen.dart
@@ -139,12 +139,15 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
 
                           setState(() {});
                         },
-                        child: NoteCard(
-                          noteId: data.noteId,
-                          title: data.title,
-                          content: data.content,
-                          color: data.color,
-                          date: data.dateToString(),
+                        child: Hero(
+                          tag: data.noteId,
+                          child: NoteCard(
+                            noteId: data.noteId,
+                            title: data.title,
+                            content: data.content,
+                            color: data.color,
+                            date: data.dateToString(),
+                          ),
                         ),
                       );
                     },
