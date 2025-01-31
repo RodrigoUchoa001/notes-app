8b9c98843109e154357a6a1f2599145f6091cf53
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-30 21:03:42 -0300

    created shimmer effect widget for the note list

---
+++ lib/ui/widgets/home_screen/shimmer_note_list.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:waterfall_flow/waterfall_flow.dart';
+
+class ShimmerNoteList extends StatelessWidget {
+  const ShimmerNoteList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WaterfallFlow.builder(
+      gridDelegate: const SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 12,
+      ),
+      itemCount: 7,
+      itemBuilder: (context, index) {
+        return Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[800]!,
+          child: Container(
+            height: Random().nextInt(100) + 100, // size between 100 and 200
+            decoration: BoxDecoration(
+              color: Colors.white.withAlpha(150),
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color: Colors.white24,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
