bf82fb492bf9627561840637dcbdfef5357443ae
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-30 22:38:15 -0300

    improved the getContrastingTextColor function

--- lib/utils.dart
+++ lib/utils.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 // get a contransting color to use in texts
 Color getContrastingTextColor(Color color) {
   double luminance = color.computeLuminance();
-  return (luminance + 0.1) > 0.5 ? Colors.black : Colors.white;
+  return (luminance + 0.15) > 0.5 ? Colors.black : Colors.white;
 }
