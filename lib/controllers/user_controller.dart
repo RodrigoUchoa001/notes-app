e10bdca5c443e80cba120a07eb75fd58fe830ed7
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-01-31 23:08:11 -0300

    now the login controller saves user info on firestore

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -9,20 +10,36 @@ class UserController {
   // codes.
   static Future<User?> loginWithGoogle() async {
     final googleAccount = await GoogleSignIn().signIn();
+    if (googleAccount == null) return null;
 
-    final googleAuth = await googleAccount?.authentication;
+    final googleAuth = await googleAccount.authentication;
 
     final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
     );
 
     final userCredential = await FirebaseAuth.instance.signInWithCredential(
       credential,
     );
 
-    user = userCredential.user;
-    return userCredential.user;
+    final user = userCredential.user;
+    if (user == null) return null;
+
+    final firestore = FirebaseFirestore.instance;
+    final userRef = firestore.collection('Users').doc(user.uid);
+
+    final userSnapshot = await userRef.get();
+    if (!userSnapshot.exists) {
+      await userRef.set({
+        'email': user.email,
+        'name': user.displayName ?? 'No Name',
+        'photoURL': user.photoURL ?? '',
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+    }
+
+    return user;
   }
 
   static Future<void> signOut() async {
