682690a936612d73062d0055d8230c2553978bda
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-01 20:59:53 -0300

    created note provider

---
+++ lib/ui/providers/note_provider.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes_app/controllers/note_controller.dart';
+
+final userProvider =
+    StreamProvider<User?>((ref) => FirebaseAuth.instance.authStateChanges());
+
+final notesStreamProvider = StreamProvider<List<NoteData>>((ref) {
+  final userAsyncValue = ref.watch(userProvider);
+
+  return userAsyncValue.when(
+    data: (user) {
+      if (user != null) {
+        return NoteController().getStreamNotes(user.uid);
+      } else {
+        return Stream.empty();
+      }
+    },
+    error: (error, _) {
+      return throw Exception(error.toString());
+    },
+    loading: () {
+      return Stream.empty();
+    },
+  );
+});
