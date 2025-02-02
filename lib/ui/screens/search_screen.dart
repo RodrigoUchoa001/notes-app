4b25a850dfe59723214477c026d46617841532b4
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-01 21:09:44 -0300

    search screen modified to work with the new note provider

--- lib/ui/screens/search_screen.dart
+++ lib/ui/screens/search_screen.dart
@@ -1,9 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:notes_app/controllers/note_controller.dart';
+import 'package:notes_app/ui/providers/note_provider.dart';
 import 'package:notes_app/ui/widgets/app_bar_button.dart';
 import 'package:notes_app/ui/widgets/home_screen/notes_list.dart';
 import 'package:notes_app/ui/widgets/home_screen/shimmer_note_list.dart';
@@ -21,32 +20,14 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
   @override
   void initState() {
     super.initState();
-    searchController.addListener(() {
-      setState(() {}); // Trigger rebuild when search text changes
-    });
+    searchController.addListener(() => setState(() {}));
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final user = snapshot.data!;
-          return _buildSearchContent(context, user);
-        } else {
-          return const Center(
-              child: CircularProgressIndicator()); // Or login prompt
-        }
-      },
-    );
-  }
-
-  Widget _buildSearchContent(BuildContext context, User user) {
-    final _notesStream = NoteController().getStreamNotes(user.uid);
+    final notesAsyncValue = ref.watch(notesStreamProvider);
 
     return Scaffold(
-      backgroundColor: const Color(0xFF252525),
       appBar: AppBar(
         backgroundColor: Color(0xFF252525),
         automaticallyImplyLeading: false,
@@ -64,7 +45,6 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
       body: SafeArea(
         minimum: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
-          // Use Column instead of ListView
           children: [
             TextFormField(
               autofocus: true,
@@ -88,36 +68,8 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
             ),
             const SizedBox(height: 16),
             Expanded(
-              // Use Expanded to fill remaining space
-              child: StreamBuilder<List<NoteData>>(
-                key: ValueKey(user.uid), // Key based on user UID
-                stream: _notesStream,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const ShimmerNoteList();
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text("ERRO: ${snapshot.error}"));
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(FontAwesomeIcons.xmark, size: 56),
-                          const SizedBox(height: 8),
-                          Text(
-                            "No notes to show!",
-                            style: GoogleFonts.nunito(
-                              color: Colors.white,
-                              fontSize: 26,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-
-                  final notes = snapshot.data!;
+              child: notesAsyncValue.when(
+                data: (notes) {
                   final filteredNotes = notes
                       .where((note) => note.title
                           .toLowerCase()
@@ -143,14 +95,15 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
                       ),
                     );
                   }
-
                   return NotesList(
                     notes: filteredNotes,
-                    onNoteUpdated: () {
-                      // No need to reload all notes
-                    },
+                    onNoteUpdated: () {},
                   );
                 },
+                loading: () => const ShimmerNoteList(),
+                error: (error, stackTrace) => Center(
+                  child: Text("ERRO: $error"),
+                ),
               ),
             ),
           ],
