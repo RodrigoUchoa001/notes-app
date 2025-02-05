81343e2d6827b6b889ee53f0dd3ce2e245dec7d4
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-04 22:22:54 -0300

    altered ui that show the list of collaborators of a note, with button to remove a collaborator

--- lib/ui/screens/edit_note_screen.dart
+++ lib/ui/screens/edit_note_screen.dart
@@ -258,95 +258,136 @@ class _EditNoteScreenState extends ConsumerState<EditNoteScreen> {
     return showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          actionsOverflowAlignment: OverflowBarAlignment.start,
-          title: Text(
-            'Manage Collaborators',
-            style: GoogleFonts.nunito(
-              color: Colors.white,
-            ),
-          ),
-          actions: [
-            Text(
-              'Collaborators List:',
-              style: GoogleFonts.nunito(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
-            ),
-            FutureBuilder(
-              future: noteData.getCollaboratorsNames(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (snapshot.hasError) {
-                  return const Text('ERROR');
-                } else if (snapshot.data!.isEmpty) {
-                  return const Text('EMPTY');
-                }
-                final data = snapshot.data;
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: data!.map(
-                    (collabName) {
-                      return Text(
-                        "\u2022 $collabName",
-                        style: GoogleFonts.nunito(
-                          color: Colors.white,
-                        ),
-                      );
-                    },
-                  ).toList(),
-                );
-              },
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Insert an e-mail below to invite it to collaborate:',
-              style: GoogleFonts.nunito(
-                color: Colors.white,
-              ),
-            ),
-            TextFormField(
-              autofocus: true,
-              decoration: InputDecoration(
-                hintText: 'Insert a e-mail...',
-              ),
-              maxLines: null,
-              controller: collabController,
-              style: GoogleFonts.nunito(
-                color: Colors.white,
-                fontSize: 16,
-              ),
-            ),
-            const SizedBox(height: 8),
-            FilledButton(
-              onPressed: () async {
-                try {
-                  await NoteController().inviteCollaborator(
-                      noteData.noteId!, user!.uid, collabController.text);
-
-                  Navigator.of(context).pop();
-                  Fluttertoast.showToast(
-                    msg: "${collabController.text} invited to collaborate.",
-                  );
-                } on Exception {
-                  Fluttertoast.showToast(
-                    msg: "User not found!",
-                  );
-                }
-              },
-              child: Text(
-                'Invite',
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              actionsOverflowAlignment: OverflowBarAlignment.start,
+              title: Text(
+                'Manage Collaborators',
                 style: GoogleFonts.nunito(
-                  fontSize: 16,
+                  color: Colors.white,
                 ),
               ),
-            ),
-          ],
+              actions: [
+                Text(
+                  'Collaborators List:',
+                  style: GoogleFonts.nunito(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                StreamBuilder(
+                  stream: noteData.getCollaboratorsNames(user!.uid),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text('ERROR: ${snapshot.error.toString()}');
+                    } else if (snapshot.data!.isEmpty) {
+                      return const Text('EMPTY');
+                    }
+                    final data = snapshot.data;
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: data!.map(
+                        (collabName) {
+                          return Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 16),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                style: BorderStyle.solid,
+                                color: Colors.white30,
+                              ),
+                            ),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    user.displayName == collabName
+                                        ? "$collabName (You)"
+                                        : collabName,
+                                    style: GoogleFonts.nunito(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                                user.displayName == collabName
+                                    ? SizedBox()
+                                    : IconButton(
+                                        tooltip: 'Remove collaborator',
+                                        onPressed: () {
+                                          setState(() {
+                                            noteData.removeCollaborator(
+                                                noteData.noteId!, collabName);
+                                          });
+
+                                          Navigator.pop(context);
+                                          Fluttertoast.showToast(
+                                            msg:
+                                                "$collabName removed from collaborators list.",
+                                          );
+                                        },
+                                        icon: Icon(FontAwesomeIcons.xmark),
+                                      ),
+                              ],
+                            ),
+                          );
+                        },
+                      ).toList(),
+                    );
+                  },
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Insert an e-mail below to invite it to collaborate:',
+                  style: GoogleFonts.nunito(
+                    color: Colors.white,
+                  ),
+                ),
+                TextFormField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    hintText: 'Insert a e-mail...',
+                  ),
+                  maxLines: null,
+                  controller: collabController,
+                  style: GoogleFonts.nunito(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                FilledButton(
+                  onPressed: () async {
+                    try {
+                      await NoteController().inviteCollaborator(
+                          noteData.noteId!, user!.uid, collabController.text);
+
+                      Fluttertoast.showToast(
+                        msg: "${collabController.text} invited to collaborate.",
+                      );
+                      collabController.text = "";
+                    } on Exception {
+                      Fluttertoast.showToast(
+                        msg: "User not found!",
+                      );
+                    }
+                  },
+                  child: Text(
+                    'Invite',
+                    style: GoogleFonts.nunito(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
         );
       },
     );
